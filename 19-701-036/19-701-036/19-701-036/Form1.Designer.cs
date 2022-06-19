1ad23f99e5cb8609ad22f26ba15734bec2a9693c
SONGUR <104678300+SONGUR@users.noreply.github.com>
2022-06-20 00:00:11 +0300

    Add files via upload

---
+++ 19-701-036/19-701-036/19-701-036/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCarName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCarModel = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCarPrice = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(190, 15);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(278, 26);
+            this.txtID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Araba Id Giriniz:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(487, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Araba Bul";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Araba Adı Giriniz:";
+            // 
+            // txtCarName
+            // 
+            this.txtCarName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCarName.Location = new System.Drawing.Point(190, 65);
+            this.txtCarName.Name = "txtCarName";
+            this.txtCarName.Size = new System.Drawing.Size(278, 26);
+            this.txtCarName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Araba Modeli Giriniz:";
+            // 
+            // txtCarModel
+            // 
+            this.txtCarModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCarModel.Location = new System.Drawing.Point(190, 117);
+            this.txtCarModel.Name = "txtCarModel";
+            this.txtCarModel.Size = new System.Drawing.Size(278, 26);
+            this.txtCarModel.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Araba Fiyatı Giriniz:";
+            // 
+            // txtCarPrice
+            // 
+            this.txtCarPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCarPrice.Location = new System.Drawing.Point(190, 172);
+            this.txtCarPrice.Name = "txtCarPrice";
+            this.txtCarPrice.Size = new System.Drawing.Size(278, 26);
+            this.txtCarPrice.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(109, 247);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(108, 46);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "KAYDET";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(275, 247);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(109, 46);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "GÜNCELLE";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(442, 247);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 46);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "SİL";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 299);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(616, 301);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(408, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "NOT: İstediğiniz arabayı bulmak için, bir veya daha fazla textboxu doldurarak tar" +
+    "atınız.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 612);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCarPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCarModel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCarName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
+            this.Text = "Araba Galerisi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCarName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCarModel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCarPrice;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 19-701-036/19-701-036/19-701-036/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Omer\Desktop\WindowsFormsApp1\WindowsFormsApp1\Database1.mdf;Integrated Security=True");
+            cn.Open();
+            GetAllCar();
+        }
+
+        private void GetAllCar()
+        {
+            cmd = new SqlCommand("Select * from CarOperation", cn);
+            
+            da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text != "")
+            {
+                SaveInfo();
+            }
+            else
+            {
+                MessageBox.Show("Id boş olamaz!");
+                txtID.Focus();
+            }
+
+            GetAllCar();
+        }
+
+        
+        protected void SaveInfo()
+        {
+            string QUERY = "INSERT INTO CarOperation " +
+                "(Carid, CarName, CarModel, CarPrice)" +
+                "VALUES (@Carid, @CarName, @CarModel, @CarPrice)";
+
+            SqlCommand cmd = new SqlCommand(QUERY, cn);
+            cmd.Parameters.AddWithValue("@Carid", txtID.Text);
+            cmd.Parameters.AddWithValue("@CarName", txtCarName.Text);
+            cmd.Parameters.AddWithValue("@CarModel", txtCarModel.Text);
+            cmd.Parameters.AddWithValue("@CarPrice", txtCarPrice.Text);
+            cmd.ExecuteNonQuery();
+        }
+
+        protected void UpdateInfo()
+        {
+            string QUERY = "Update CarOperation" +
+                "Set CarName = @CarName, CarModel = @CarModel, CarPrice = @CarPrice" +
+                "where Carid = @Carid";
+
+            SqlCommand cmd = new SqlCommand(QUERY, cn);
+            cmd.Parameters.AddWithValue("@Carid", txtID.Text);
+            cmd.Parameters.AddWithValue("@CarName", txtCarName.Text);
+            cmd.Parameters.AddWithValue("@CarModel", txtCarModel.Text);
+            cmd.Parameters.AddWithValue("@CarPrice", txtCarPrice.Text);
+            cmd.ExecuteNonQuery();
+        }
+
+        protected void DeleteInfo()
+        {
+            string QUERY = "Delete from CarOperation " +
+                "where Carid = @Carid";
+            SqlCommand cmd = new SqlCommand(QUERY, cn);
+            cmd.Parameters.AddWithValue("@Carid", txtID.Text);
+            cmd.ExecuteNonQuery ();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("Select * from CarOperation where Carid=" + txtID.Text, cn);
+            da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource= dt;
+            
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateInfo();
+            GetAllCar();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DeleteInfo();
+            GetAllCar();
+        }
+    }
+}
